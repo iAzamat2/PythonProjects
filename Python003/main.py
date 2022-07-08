926ecbb3bad09ac53ca76604de408e51885c3835
Azamat <isabirovazamati@gmail.com>
2022-07-08 23:55:04 +0500

    Add Python 003 Ввод числа из диапозона

---
+++ Python003/main.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input('Введите число '))
+    if number > 0 and number < 10:
+        print(number**2)
+        break
+    else:
+        print('Число должно быть > 0 и < 10')
